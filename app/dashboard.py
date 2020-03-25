65dacc829c54171842a5151104757d96ec049fa4
Cleber Vieira <clebervieira@gmail.com>
2020-03-25 15:17:39 -0700

    added form to submit qso

--- app/dashboard.py
+++ app/dashboard.py
@@ -3,7 +3,7 @@ import flask
 
 from app import app
 
-from flask import render_template
+from flask import render_template, request
 
 
 @app.route("/admin/dashboard")
@@ -13,6 +13,23 @@ def dashboard():
 
 @app.route("/admin/addqso")
 def addqso():
+    return render_template("/dashboard/addqso.html")
+
+#TODO: combine submitqso_form and submitqso(api), also prevent form from submitting blank data
+@app.route('/submitqso_form', methods=["GET", "POST"])
+def submitqso_form():
+    log = logQSO.LogNewQSO("call_sign", "signal_sent", "signal_received")
+
+    if request.method == "POST":
+
+        req = request.form
+
+        callSign = req["callSign"]
+        signalSent = req.get("signalSent")
+        signalReceived = request.form["signalReceived"]
+
+        log.write_qso(callSign, signalSent, signalReceived)
+
     return render_template("/dashboard/addqso.html")
 
 
--- app/views.py
+++ app/views.py
@@ -11,6 +11,7 @@ def index():
 def qso():
     log = logQSO.LogNewQSO("call_sign", "signal_sent", "signal_received")
     view_log = log.build_string()
+
     return render_template("/public/qso.html", view_log=view_log)
 
 @app.route('/readqso', methods=['GET'])
