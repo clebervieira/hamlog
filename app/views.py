068b346aa8a21249359bc680123eba5229f99786
Cleber Vieira <clebervieira@gmail.com>
2020-03-24 20:42:50 -0700

    navbar + qso page

--- app/views.py
+++ app/views.py
@@ -5,3 +5,7 @@ from flask import render_template
 @app.route("/")
 def index():
         return render_template("/public/index.html")
+
+@app.route("/qso")
+def qso():
+        return render_template("/public/qso.html")
