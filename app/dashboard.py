480d2cf85342ac348a298dbaa2c633832a78882d
Cleber Vieira <clebervieira@gmail.com>
2020-04-01 08:24:25 -0700

    authentication complete

--- app/dashboard.py
+++ app/dashboard.py
@@ -5,14 +5,17 @@ from app import app, db, bcrypt
 from app.forms import RegistrationForm, LoginForm
 from app.models import User, Post
 from flask import render_template, request, url_for, flash, redirect
+from flask_login import login_user, current_user, logout_user, login_required
 
 
 @app.route("/admin/dashboard")
+@login_required
 def dashboard():
     return render_template("/dashboard/dashboard.html", title='Dashboard')
 
 
 @app.route("/admin/addqso")
+@login_required
 def addqso():
     return render_template("/dashboard/addqso.html", title='QSO Forms')
 
@@ -45,6 +48,8 @@ def submitqso():
 
 @app.route("/admin/register", methods=['GET', 'POST'])
 def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('dashboard'))
     form = RegistrationForm()
     if form.validate_on_submit():
         hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
@@ -58,11 +63,20 @@ def register():
 
 @app.route("/admin/login", methods=['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('dashboard'))
     form = LoginForm()
     if form.validate_on_submit():
-        if form.email.data == 'clebervieira@gmail.com' and form.password.data == 'chiclete':
-            flash('You have been logged in!', 'success')
-            return redirect(url_for('dashboard'))
+        user = User.query.filter_by(email=form.email.data).first()
+        if user and bcrypt.check_password_hash(user.password, form.password.data):
+            login_user(user, remember=form.remember.data)
+            next_page = request.args.get('next')
+            return redirect(next_page) if next_page else redirect(url_for('dashboard'))
         else:
             flash('Login Unsuccessful. Please check username and password', 'danger')
     return render_template('/dashboard/login.html', title='Login', form=form)
+
+@app.route("/admin/logout")
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
